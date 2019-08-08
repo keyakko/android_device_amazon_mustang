cedaeb3fd09e66dc2c5e59c67e414c20312caa33
keyakko <keyakko.web@gmail.com>
2019-08-08 13:05:37 +0000

    init

---
+++ Android.mk
@@ -0,0 +1,9 @@
+ifneq ($(filter mustang, $(TARGET_DEVICE)),)
+
+LOCAL_PATH := $(call my-dir)
+
+# if some modules are built directly from this directory (not subdirectories),
+# their rules should be written here.
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
+endif
