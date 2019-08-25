1f32dcda3facfd477244750218583883868c616d
root <root@builder.us-east1-b.c.keyakko-android-builder.internal>
2019-08-25 16:30:13 +0000

    Cleanup

--- Android.mk
+++
@@ -1,9 +0,0 @@
-ifneq ($(filter mustang, $(TARGET_DEVICE)),)
-
-LOCAL_PATH := $(call my-dir)
-
-# if some modules are built directly from this directory (not subdirectories),
-# their rules should be written here.
-
-include $(call all-makefiles-under,$(LOCAL_PATH))
-endif
