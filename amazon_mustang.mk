1b72a550428d1feb7739a97b1183de8f355e5f34
root <root@builder.us-east1-b.c.keyakko-android-builder.internal>
2019-08-25 16:30:58 +0000

    Update

---
+++ amazon_mustang.mk
@@ -0,0 +1,13 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+PRODUCT_NAME := amazon_mustang
+PRODUCT_DEVICE := mustang
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := AOSP on mustang
+PRODUCT_MANUFACTURER := Amazon
+PRODUCT_RESTRICT_VENDOR_FILES := true
+
+# Inherit from hardware-specific part of the product configuration
+$(call inherit-product, device/amazon/mustang/device.mk)
+$(call inherit-product-if-exists, vendor/amazon/mustang/device-vendor.mk)
+#$(call inherit-product-if-exists, vendor/qcom/proprietary/common/config/device-vendor.mk)
---
+++ vendorsetup.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+add_lunch_combo amazon_mustang-userdebug
